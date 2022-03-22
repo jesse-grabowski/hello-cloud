2b06ce499bc37625084f35f11d8dd1a84cf26e22
Jesse Grabowski <jssgrabowski@gmail.com>
2022-03-22 08:17:36 -0400

    Add publish-release action

---
+++ src/main/java/com/jessegrabowski/hellocloud/greetings/GreetingsController.java
@@ -0,0 +1,17 @@
+package com.jessegrabowski.hellocloud.greetings;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingsController {
+
+    @GetMapping(path = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+    public @ResponseBody String hello(@RequestParam(required = false, name = "name", defaultValue = "World") String name) {
+        return "Hello, ".concat(name);
+    }
+
+}
